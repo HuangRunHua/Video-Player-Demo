1b78e2829c6f34161054f50fd812fc38ae8a96fd
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2021-06-20 13:17:07 +0800

    Initial Commit

---
+++ Enjoy Video Together/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Enjoy Video Together
+//
+//  Created by Runhua Huang on 2021/6/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Enjoy Video Together/Enjoy_Video_TogetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Enjoy_Video_TogetherApp.swift
+//  Enjoy Video Together
+//
+//  Created by Runhua Huang on 2021/6/20.
+//
+
+import SwiftUI
+
+@main
+struct Enjoy_Video_TogetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
